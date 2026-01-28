93739c8f42d1a8e06a1b9216507668cc42b2037c
Mshahwaz <shahwazkh47@gmail.com>
2026-01-28 11:12:45 +0530

    addinf maintiner and some polycies:v4

--- vars/deploycontainer.groovy
+++ vars/deploycontainer.groovy
@@ -1,4 +1,4 @@
 def call() {
     sh ' docker rm $REPO || true '
-    sh ' docker run --restart unless-stopped -d -p $PORT:$PORT $REPO:$TAG '
+    sh ' docker run --restart unless-stopped --name $REPO -d -p $PORT:$PORT $REPO:$TAG '
 }
